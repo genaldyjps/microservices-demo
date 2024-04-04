6419a231397f04080351b9da8b38720e2efef2ab
genaldy <genaldy.jiwari@gmail.com>
2024-04-04 16:45:18 +0700

    add kafka to elastic service

---
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.microservices.demo.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain webSecurityCustomizer(HttpSecurity http) throws Exception {
+        http.
+                cors(Customizer.withDefaults()).
+                csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/actuator/**"),
+                                new AntPathRequestMatcher("/encrypt/**"),
+                                new AntPathRequestMatcher("/decrypt/**"))
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
